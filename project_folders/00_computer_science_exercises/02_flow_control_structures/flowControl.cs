aeb24e152ea1cbf3a5835bc6f7f4f60d3facf251
MrAltYT <s20100391@students.duvalschools.org>
2024-09-04 13:45:04 -0400

    returned code here!

---
+++ project_folders/00_computer_science_exercises/02_flow_control_structures/flowControl.cs
@@ -0,0 +1,121 @@
+// Johnson Traevon, FlowControls 8/28/2024 v0.1
+using System;
+class Template {
+  static void Main() {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // int day = 1;
+    // switch(day)
+    // {
+    //   case 1:
+    //     Console.WriteLine("It's Monday.\n");
+    //     break;
+    //   case 2:
+    //     Console.WriteLine("It's Tuesday.\n");
+    //     break;
+    //   case 3:
+    //     Console.WriteLine("It's Wednesday.\n");
+    //     break;
+    //   case 4:
+    //     Console.WriteLine("It's Thursday.\n");
+    //     break;
+    //   case 5:
+    //     Console.WriteLine("It's Friday. Lets goo\n");
+    //     break;
+    //   case 6:
+    //     Console.WriteLine("It's Saturday, have fun");
+    //     break;
+    //   case 7:
+    //     Console.WriteLine("It's Sunday, go sleep up.\n");
+    //     break;
+
+    // }
+    
+    // Repeating Steps over, and over, and over again
+    // Loops are used to repeat instructions multiple times
+
+    // white loops are "musical chairs" in computer science
+    // the code will run as long as the condition is true.
+    // while loop syntax:
+    /* while (CONDITIONAL_EXPRESSION) DOES NOT NEED COLON
+    {
+       Code to loop goes here.
+    }
+    */
+    
+    // int z = 0;
+    // while (z <= 100)
+    // {
+    //   Console.WriteLine(z);
+    //   z++;
+    // }
+
+    int y = 100;
+    while (y != -1)
+    {
+       Console.WriteLine(y);
+       y--;
+    }
+    /* 
+    for (Statement1; statement2;  statement3)
+    {
+      Code to run through the loop
+    }
+    statement1 = code to run before loop starts 1 Time!!
+    statement2 = conditional expression to check to see each time before loop starts
+    statement3 =  code to run after each loop
+
+    each 'trip' through the loop is called iteration
+    */
+
+    // Counting to 10 with for loops
+    for (int i = 10; i != -1; i--)
+    {
+      Console.WriteLine(i);
+    }
+    
+    
+
+
+
+    
+  }
+}
