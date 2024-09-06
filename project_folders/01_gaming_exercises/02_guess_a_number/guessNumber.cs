1c6db000c3016d4abcc0adc4de81381c81d886f8
MrAltYT <s20100391@students.duvalschools.org>
2024-09-06 13:57:57 -0400

    v0.0
    numbguess

---
+++ project_folders/01_gaming_exercises/02_guess_a_number/guessNumber.cs
@@ -0,0 +1,22 @@
+// 4A Johnson Traevon, Number Guess, v0.0
+using System;
+class GuessNumber {
+  static void Main() {
+    int numGuess = 0;
+    int maxGuess = 5;
+
+    // Generate the secret number here.
+    // while loop
+      // Allow the user to guess the number.
+      // Compare the guess to the secret number.
+      // If match, print win, if > or <, give hint.
+      // +1 to numGuess
+      // Check if run out of guesses (NumGuess > Max Guesses)
+      // If true print a "you lose" message, if false guess again.
+
+
+
+
+      
+  }
+}
