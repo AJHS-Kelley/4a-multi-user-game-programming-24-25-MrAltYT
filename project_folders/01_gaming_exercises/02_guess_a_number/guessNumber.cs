6d78b359e26252711787fba4bb448cbfa9d4fbf5
MrAltYT <s20100391@students.duvalschools.org>
2024-09-06 14:21:47 -0400

    v0.1

--- project_folders/01_gaming_exercises/02_guess_a_number/guessNumber.cs
+++ project_folders/01_gaming_exercises/02_guess_a_number/guessNumber.cs
@@ -1,12 +1,22 @@
-// 4A Johnson Traevon, Number Guess, v0.0
+// 4A Johnson Traevon, Number Guess, v0.1
 using System;
 class GuessNumber {
   static void Main() {
     int numGuess = 0;
     int maxGuess = 5;
+    int guess;
 
     // Generate the secret number here.
-    // while loop
+    Random rnd =  new Random(); // Create an object named 'rng' that is a copy of the Random() class.
+    int hehehehaw = rnd.Next(1, 1001); // generate from 1 to 9 (MIN to MAX)
+
+    while (numGuess < maxGuess) {
+      Console.WriteLine("Please guess a number between 1 through 1000");
+      guess = Convert.ToInt32(Console.ReadLine());
+      Console.WriteLine(guess);
+      numGuess++;
+
+    }
       // Allow the user to guess the number.
       // Compare the guess to the secret number.
       // If match, print win, if > or <, give hint.
