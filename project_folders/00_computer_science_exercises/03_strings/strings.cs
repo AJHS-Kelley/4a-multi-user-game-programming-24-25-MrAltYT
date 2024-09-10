340820bdea2dd70618e3039e87b704a3ed1ab462
MrAltYT <s20100391@students.duvalschools.org>
2024-09-10 14:54:01 -0400

    Update strings.cs

--- project_folders/00_computer_science_exercises/03_strings/strings.cs
+++ project_folders/00_computer_science_exercises/03_strings/strings.cs
@@ -1,4 +1,4 @@
-// Johnson Traevon strings C# v0.1
+// Johnson Traevon strings C# v0.2
 using System;
 class Template {
   static void Main() {
@@ -22,6 +22,32 @@ class Template {
 
     Console.WriteLine(greeting.ToUpper()); // make entire string upper case
     Console.WriteLine(goodbye.ToLower()); // make entire string lower case
+
+    // String Concatenation "Con Cat Uh Nation"
+    string comboString = greeting + goodbye;
+    Console.WriteLine(comboString);
+
+    // String Concatenation Method 2
+    string comboString2 = string.Concat(greeting, goodbye,);
+    Console.WriteLine(comboString2);
+
+    // String Interpolation --  Subsituting variables into strings
+    string comboString3 = $"My greeting is {greeting} and my goodbye is {goodbye}";
+    Console.WriteLine(comboString3);
+
+    // String Interpolation --  Subsituting variables into strings Method 2
+    Console.WriteLine("My greeting is " + comboString3 + "and my goodbye is " + goodbye + "\n.");
+
+    //Accessing parts of strings
+    // index is a specific location of a character in a strong
+    // ALL string indexes start at
+     
+     Console.WriteLine(goodbye[0]); // Print the character at index 0 
+     Console.WriteLine(goodbye[4]); // print the 5th character in your index
+     
+     // Where is it in my string?
+     Console.WriteLine(greeting.IndexOf("y"));
+
 
 
   
