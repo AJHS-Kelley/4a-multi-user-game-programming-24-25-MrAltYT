ca63623969f24e17edb7a0c0aab491f514659db7
MrAltYT <s20100391@students.duvalschools.org>
2024-09-18 13:55:45 -0400

    v0.1
    Rules Of Game

--- project_folders/01_gaming_exercises/04_rock_paper_scissors/rock-paper-scissors.cs
+++ project_folders/01_gaming_exercises/04_rock_paper_scissors/rock-paper-scissors.cs
@@ -6,28 +6,34 @@ class Template {
     // VARIABLES
     bool gameplay =  false;
     string playerName = "";
-    string answerTPN = "";
+    string testPlayer = "";
     int playerScore;
     int cpuScore;
     string playerChoice = "";
     string cpuChoice = "";
 
     // Player Name
-    Console.WriteLine("By RUBY-CHAN\n\n\n");
+
+    Console.WriteLine("By ❤️ ⭐ RUBY-CHAN⭐ ❤️\n\n\n");
     Console.WriteLine("Please.. Input.. YOUR NAMEEEE\n");
     playerName = (Console.ReadLine());
     Console.WriteLine("Is this YOUR NAME? " + playerName + "? \n\n If True, type yes");
-    answerTPN = Console.ReadLine().ToLower();
-    if (answerTPN == "yes")
+    testPlayer = Console.ReadLine().ToLower();
+    if (testPlayer == "yes")
     {
         gameplay = true;
     }
     else 
     {
-        Console.WriteLine("You've ran into an error, please restart the game.");
+        Console.WriteLine("You've ran into an error, please restart the game.\n");
     }
 
-    // Start of Game
+    // Rules of Game
+
+    Console.WriteLine("Rock beats Scissors, Scissors beats Paper, and Paper beats Rock\n");
+    Console.WriteLine("The winner scores a point. If each player picks the same no points are awarded\n");
+    Console.WriteLine("The first player to five points wins.\n");
+
 
   }
 }
